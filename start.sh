2a02f71234a751775a6b5aeb2f37a622086949c7
Amirul Islam <amirulandalib9@gmail.com>
2021-11-03 21:07:45 +0600

    #shelltoolkit
    Co-Authored-By: Anas <77075674+anasty17@users.noreply.github.com>
    Co-Authored-By: Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
    Co-Authored-By: FA Tulloh <77918734+yourtulloh@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,13 +1,22 @@
 if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
 	echo "Rclone config detected"
-	echo "[DRIVE]" > rclone.conf
-    mkdir -p /app/.config/rclone
-	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> /app/.config/rclone/rclone.conf
+    mkdir -p /usr/src/app/.config/rclone
+	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> /usr/src/app/.config/rclone/rclone.conf
 fi
 
 if [[ -n $BOT_TOKEN && -n $OWNER_ID ]]; then
 	echo "Bot token and owner ID detected"
 	python3 config.py
+fi
+
+if [[ -n $CREDENTIALS_LINK ]]; then
+	echo "credentials.json detected"
+    aria2c $CREDENTIALS_LINK && drivedl set /usr/src/app/credentials.json
+fi
+
+if [[ -n $ACCOUNTS_FOLDER_LINK ]]; then
+	echo "accounts.zip detected"
+    aria2c $ACCOUNTS_FOLDER_LINK && unzip accounts.zip -d accounts && rm *.zip
 fi
 
 echo "SETUP COMPLETED"
